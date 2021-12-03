d9aacbf9a4ccb07cb41b45278cf668945c370732
kaniket7209 <kaniket7209@gmail.com>
2021-12-03 11:54:58 +0530

    update

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
+import os
 import datetime
 from flask import Flask, render_template, request
 from pymongo import MongoClient
+from dotenv import load_dotenv
 
+load_dotenv()
 
 def create_app():
     app = Flask(__name__)
-    client = MongoClient(
-        "mongodb+srv://niklite:niklite@cluster0.unse2.mongodb.net/test")
+    client = MongoClient(os.environ.get("MONGODB_URI"))
     app.db = client.microBlog
 
     # entries = []
